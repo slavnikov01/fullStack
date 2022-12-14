1d2d267f4a0f7436ef3fd51d58a0cd9a1469f1ff
Slavnikov Bogdan <baizy228@mail.ru>
2022-12-14 22:56:33 +1000

    main

--- client/src/app/layouts/main.jsx
+++ client/src/app/layouts/main.jsx
@@ -1,24 +1,15 @@
 import React from "react";
-import useMockData from "../utils/mockData";
+// import useMockData from "../utils/mockData";
 
 const Main = () => {
-    const { error, initialize, progress, status } = useMockData();
-    const handleClick = () => {
-        initialize();
-    };
     return (
         <div className="container mt-5">
-            <h1> Main Page</h1>
-            <h3>Инициализация данных в FireBase</h3>
-            <ul>
-                <li>Status:{status}</li>
-                <li>Progress: {progress}%</li>
-                {error && <li>error: {error}</li>}
-            </ul>
-            <button className="btn btn-primary" onClick={handleClick}>
-                {" "}
-                Инициализировать
-            </button>
+            <h1> Сайт для поиска друзей <i className="bi bi-chat-square-dots-fill"></i>
+</h1>
+            <br></br>
+            <h3>Хочешь пообщаться и найти друзей, но не знаешь как?</h3>
+            <h3>Наш сайт поможет тебе! Регистрируйся и начинай общаться прямо сейчас!</h3>
+
         </div>
     );
 };
